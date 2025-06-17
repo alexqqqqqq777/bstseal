df6150f59037ad6a593777ba16a237c5f5d38da3
Oleksandr <oleksandr@MacBook-Air-Oleksandr.local>
2025-06-17 11:45:12 +0300

    ffi: set library name to bstseal for node bindings

--- bindings/node/build.js
+++ bindings/node/build.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 try {
   console.log('Building bstseal-ffi ...');
-  execSync('cargo build -p bstseal-ffi --release', { stdio: 'inherit', cwd: path.resolve(__dirname, '..', '..') });
+  execSync('cargo build -p bstseal-ffi --release --lib', { stdio: 'inherit', cwd: path.resolve(__dirname, '..', '..') });
   console.log('bstseal-ffi build complete');
 } catch (e) {
   console.error('Failed to build bstseal-ffi');
